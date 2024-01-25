e54e66d4f60097f572950e5e24e68c8aa472ea5a
safidy54 <safidimalalarabezatovo@gmail.com>
2024-01-25 18:27:48 +0000

    Up

--- src/main/java/com/voiture/gasicar/Controller/UserController.java
+++ src/main/java/com/voiture/gasicar/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.voiture.gasicar.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserController {
     @Autowired
     JwtUtils utils;
 
-    @PostMapping("/signup")
+    @GetMapping("/signup")
     public ResponseEntity<String> signup(HttpServletRequest request) {
         try {
             User user = new User();
