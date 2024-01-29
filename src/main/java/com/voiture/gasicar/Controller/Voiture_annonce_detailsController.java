461b36a44a5b20cc13596f2213bcf7953ab60e39
safidy54 <safidimalalarabezatovo@gmail.com>
2024-01-29 12:58:28 +0000

    back-v21

--- src/main/java/com/voiture/gasicar/Controller/Voiture_annonce_detailsController.java
+++ src/main/java/com/voiture/gasicar/Controller/Voiture_annonce_detailsController.java
@@ -10,15 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import com.voiture.gasicar.Model.Voiture_annonce_details;
 import com.voiture.gasicar.Security.Authority;
 import com.voiture.gasicar.Security.Role;
-import com.voiture.gasicar.Service.Voiture_annonce_detailService;
 
 @RestController
 @RequestMapping("/api/annonce_details")
 public class Voiture_annonce_detailsController {
     
-    @Autowired
-    private Voiture_annonce_detailService vaservice;
-
     @GetMapping("/voir")
     public Vector<Voiture_annonce_details> getAllVoitureAnnonce_details() throws Exception{
         Vector<Voiture_annonce_details> vad = new Voiture_annonce_details().select(null);
