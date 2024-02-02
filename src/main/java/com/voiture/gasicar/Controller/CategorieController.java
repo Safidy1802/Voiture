7ea1f1535f5f8cd53a382356b69b4044e3b8be9e
safidy54 <safidimalalarabezatovo@gmail.com>
2024-02-02 12:21:19 +0000

    Update CategorieController.java

--- src/main/java/com/voiture/gasicar/Controller/CategorieController.java
+++ src/main/java/com/voiture/gasicar/Controller/CategorieController.java
@@ -19,7 +19,6 @@ public class CategorieController {
     private CategorieService CategorieService;
 
     @GetMapping("/voir")
-    @Authority(role = Role.ADMIN)
     public List<Categorie> getAllCategories() {
         return CategorieService.getAllCategories();
     }
