bdb086a27c1719c064ec6daa0defe91147dce36b
safidy54 <safidimalalarabezatovo@gmail.com>
2024-02-09 08:52:28 +0000

    back-v46

--- src/main/java/com/voiture/gasicar/Service/Photo.java
+++ src/main/java/com/voiture/gasicar/Service/Photo.java
@@ -22,12 +22,13 @@ public class Photo {
         Pattern pattern = Pattern.compile("data:image/(.*?);base64,");
         Matcher matcher = pattern.matcher(base64);
         matcher.find();
+        String format=matcher.group(1);
         base64 = base64.substring(matcher.end());
         byte[] decodedBytes = Base64.getDecoder().decode(base64);
         ByteArrayInputStream inputStream = new ByteArrayInputStream(decodedBytes);
-
+        this.setFilename(filename+"."+format);
         File outputImage = new File(filename);
-        ImageIO.write(ImageIO.read(inputStream), "jpg", outputImage);
+        ImageIO.write(ImageIO.read(inputStream), format, outputImage);
 
         return outputImage;
 
