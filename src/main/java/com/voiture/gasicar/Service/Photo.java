03e3a2f6f0a0c8d66d1cfcaded9fab948d8751ef
safidy54 <safidimalalarabezatovo@gmail.com>
2024-02-09 05:22:13 +0000

    Update Photo.java

--- src/main/java/com/voiture/gasicar/Service/Photo.java
+++ src/main/java/com/voiture/gasicar/Service/Photo.java
@@ -21,7 +21,7 @@ public class Photo {
     public File convertToFile() throws IOException {
         Pattern pattern = Pattern.compile("data:image/(.*?);base64,");
         Matcher matcher = pattern.matcher(base64);
-        if (matcher.find()) {
+        matcher.find();
             base64 = base64.substring(matcher.end());
             byte[] decodedBytes = Base64.getDecoder().decode(base64);
             ByteArrayInputStream inputStream = new ByteArrayInputStream(decodedBytes);
@@ -30,11 +30,8 @@ public class Photo {
             ImageIO.write(ImageIO.read(inputStream), "jpg", outputImage);
             
             return outputImage;
-        } else {
-            throw new IllegalStateException("No match available");
-        }
+        
     }
-    
 
 
     public String getBase64() {
